2135e7ff620e404cfeaaa7ac831b08b87f00f683
Davis <davisjoseph767@gmail.com>
2023-09-04 09:15:35 -0700

    updated files

--- main.h
+++ main.h
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
