df20215878dfcdeb47fe1d5c5e3ae0cf1e286a92
seeyou7 <>
2023-09-07 03:22:39 -0700

    fix betty error on pint func

--- opcode.c
+++ opcode.c
@@ -41,10 +41,11 @@ void _pall(stack_t **stack, unsigned int line_number)
 
 	}
 }
+
 /**
- * pint - Prints the value of the top element in the stack.
- * @stack: Pointer to a pointer to the stack.
- * @line_number: The line number where the function is called.
+ *_pint - Prints the value of the top element in the stack.
+ *@stack: Pointer to a pointer to the stack.
+ *@line_number: The line number where the function is called.
  *
  */
 void _pint(stack_t **stack, unsigned int line_number)
